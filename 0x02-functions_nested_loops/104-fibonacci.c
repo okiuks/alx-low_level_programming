b4360840256d3145b374c64cd7df46ee59db0bc3
okiuks <okpako.ochuko8@gmail.com>
2023-02-23 02:55:45 -0800

    Task 14

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints fibonacci series between 1 and 98
+ * Return: 0
+ */
+int main(void)
+{
+	int num1 = 1, num2 = 2, nextNum, count = 2;
+
+	while (count < 98)
+	{
+		nextNum = num1 + num2;
+		printf("%d, ", nextNum);
+		num1 = num2;
+		num2 = nextNum;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
