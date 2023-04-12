4e0237f486d016cb0404840edddb7c2a42e5e8f6
okiuks <okpako.ochuko8@gmail.com>
2023-04-12 08:00:06 -0700

    task 5

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,13 +9,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int m;
+	unsigned int add;
 
 	if (index > 63)
 		return (-1);
 
-	m = 1 << index;
-	*n = (*n | m);
+	add = 1 << index;
+	*n = (*n | add);
 
 	return (1);
 }
